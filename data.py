d72070873509d57d13d3fea2cc9a5fb48c213ae4
Justin Lachapelle <46169956+justinlachap@users.noreply.github.com>
2021-01-02 10:43:31 -0500

    Update data.py

--- data.py
+++ data.py
@@ -5,73 +5,39 @@ import matplotlib.pyplot as plt
 import requests
 from bs4 import BeautifulSoup
 
+KEYWORDS = ['virus', 'coronavirus', 'covid-19']
+
 
 def get_words(link):
     r = requests.get(link)
     soup = BeautifulSoup(r.content, features="html.parser")
     text_div = (''.join(s.findAll(text=True)) for s in soup.findAll('ul'))
     c_div = Counter((x.rstrip(punctuation).lower() for y in text_div for x in y.split()))
-    return c_div['virus'] + c_div['coronavirus'] + c_div['covid-19']
+    return c_div[KEYWORDS[0]] + c_div[KEYWORDS[1]] + c_div[KEYWORDS[2]]
 
 
-def january():
-    for i in range(1, 32):
-        link = "https://www.lapresse.ca/archives/2020/1/%d.php" % (i)
-        list.append((i, get_words(link)))
+# days per month
+months = {'january': 31, 'feb': 29, 'march': 31, 'april': 30, 'may': 31, 'june': 30}
 
+list, index = [], 1
+for i in months.values():
+    for j in range(1, i + 1):
+        link = f"https://www.lapresse.ca/archives/2020/{index}/{j}.php"
+        list.append((j + index * i, get_words(link)))
+        index += 1
 
-def feb():
-    for i in range(1, 30):
-        link = "https://www.lapresse.ca/archives/2020/2/%d.php" % (i)
-        list.append((i + 32, get_words(link)))
-
-
-def march():
-    for i in range(1, 32):
-        link = "https://www.lapresse.ca/archives/2020/3/%d.php" % (i)
-        list.append((i + 62, get_words(link)))
-
-
-def april():
-    for i in range(1, 31):
-        link = "https://www.lapresse.ca/archives/2020/4/%d.php" % (i)
-        list.append((i + 94, get_words(link)))
-
-
-def may():
-    for i in range(1, 32):
-        link = "https://www.lapresse.ca/archives/2020/5/%d.php" % (i)
-        list.append((i + 125, get_words(link)))
-
-
-def june():
-    for i in range(1, 27):
-        link = "https://www.lapresse.ca/archives/2020/6/%d.php" % (i)
-        list.append((i + 157, get_words(link)))
-
-
-list = []
-january()
-feb()
-march()
-april()
-may()
-june()
-x = []
-y = []
+x, y = [], []
 for i in range(len(list)):
     x.append(list[i][0])
     y.append(list[i][1])
 
 plt.plot(x, y, 'r')
-plt.vlines(x=30, ymin=0, ymax=50, linestyles='--', colors='#D3D3D3')
-plt.vlines(x=60, ymin=0, ymax=50, linestyles='--', colors='#D3D3D3')
-plt.vlines(x=90, ymin=0, ymax=50, linestyles='--', colors='#D3D3D3')
-plt.vlines(x=120, ymin=0, ymax=50, linestyles='--', colors='#D3D3D3')
-plt.vlines(x=150, ymin=0, ymax=50, linestyles='--', colors='#D3D3D3')
+for i in [30, 60, 90, 120, 150]:
+    plt.vlines(x=i, ymin=0, ymax=50, linestyles='--', colors='#D3D3D3')
 plt.text(54 + 7, -10, 'Données utilisées: lapresse.ca/archives/')
 plt.xlabel('Jours de 2020')
 plt.ylabel('Nombre d\'articles par jour')
-plt.suptitle('Nombre d\'articles quotidiens à propos du Coronavirus publiés dans le journal La Presse durant la première moitié de 2020',
-             fontsize=13, y=0.96)
+plt.suptitle(
+    'Nombre d\'articles quotidiens à propos du Coronavirus publiés dans le journal La Presse durant la première moitié de 2020',
+    fontsize=13, y=0.96)
 plt.show()
